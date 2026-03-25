f05653ed973c91213e6be76a7b737f93842a506b
NaikoLeFou <joan.courty@ynov.com>
2026-03-25 15:51:07 +0100

    fix(Animal): sync hunger, disease, and aging mechanics with specs

--- Animal.cs
+++ Animal.cs
@@ -1,58 +1,102 @@
 using System;
 
-public enum Sexe {
-    Male, 
-    Femelle 
-}
-public enum EtatSante { 
-    BonneSante, 
-    Affame, 
-    Malade, 
-    Mort 
-}
+public enum Sexe { Male, Femelle }
+public enum TypeAlimentation { Viande, Graines }
 
 public abstract class Animal
 {
-    public TypeAnimal Espece { get; protected set; }
     public Sexe SexeAnimal { get; protected set; }
     public int AgeMois { get; protected set; }
-    public float JaugeFaim { get; protected set; } 
-    public EtatSante Sante { get; protected set; }
+    
+    public int JoursSansManger { get; protected set; }
+    public bool AEuFaim => JoursSansManger >= JoursAvantFaimMax; 
+    
+    // Santé
+    public bool EstMalade { get; protected set; }
+    public int JoursMaladieRestants { get; protected set; }
+    public bool EstMort { get; protected set; } 
+    
+    public bool EstEnGestation { get; protected set; }
+    public int JoursGestationRestants { get; protected set; }
 
-    public Animal(TypeAnimal espece, Sexe sexe, int ageInitial)
+    public abstract TypeAlimentation Alimentation { get; }
+    public abstract decimal ConsommationJourKg { get; }
+    public abstract int JoursAvantFaimMax { get; } 
+    public abstract int AgeMaturiteMois { get; }
+    public abstract int FinReproductionMois { get; } 
+    public abstract int EsperanceVieMois { get; } 
+
+    private Random de = new Random();
+
+    public Animal(Sexe sexe, int ageMois)
     {
-        Espece = espece;
         SexeAnimal = sexe;
-        AgeMois = ageInitial;
-        JaugeFaim = 100f; 
-        Sante = EtatSante.BonneSante;
+        AgeMois = ageMois;
+        EstMort = false;
+        JoursSansManger = 0;
     }
 
-    public void Grandir()
+    public virtual void Manger(decimal quantiteDisponible)
+    {
+        decimal besoin = ConsommationJourKg;
+        
+        if (SexeAnimal == Sexe.Femelle && EstEnGestation)
+        {
+            besoin *= 2;
+        }
+
+        if (quantiteDisponible >= besoin)
+        {
+            JoursSansManger = 0; 
+        }
+        else
+        {
+            JoursSansManger++; 
+            
+            if (EstEnGestation && AEuFaim)
+            {
+                Console.WriteLine("Fausse couche : une femelle a eu trop faim.");
+                EstEnGestation = false;
+                JoursGestationRestants = 0;
+            }
+        }
+    }
+
+    public void GererMaladie()
+    {
+        if (EstMalade)
+        {
+            JoursMaladieRestants--;
+
+            if (de.Next(1, 101) <= 10) 
+            {
+                EstMort = true;
+                Console.WriteLine("Un animal n'a pas survécu à sa maladie...");
+            }
+            else if (JoursMaladieRestants <= 0)
+            {
+                EstMalade = false;
+                Console.WriteLine("Un animal a guéri de sa maladie !");
+            }
+        }
+    }
+
+    public void VieillirUnMois()
     {
         AgeMois++;
-    }
-
-    public void AvoirFaim(float quantite)
-    {
-        JaugeFaim -= quantite;
-        if (JaugeFaim <= 0)
+        if (AgeMois >= EsperanceVieMois)
         {
-            JaugeFaim = 0;
-            Sante = EtatSante.Affame;
+            EstMort = true;
+            Console.WriteLine("Un animal est mort de vieillesse.");
         }
     }
 
-    public void Manger(float quantite)
+    public bool PeutSeReproduire()
     {
-        if (Sante == EtatSante.Mort) return;
-
-        JaugeFaim += quantite;
-        if (JaugeFaim > 100f) JaugeFaim = 100f; 
-        
-        if (Sante == EtatSante.Affame && JaugeFaim > 20f)
+        if (AEuFaim || EstMalade || AgeMois < AgeMaturiteMois || AgeMois > FinReproductionMois || EstEnGestation)
         {
-            Sante = EtatSante.BonneSante;
+            return false;
         }
+        return true;
     }
 }
