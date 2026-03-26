2eb4d875fac32f64e37e84deec1bcf86a8e7e509
NaikoLeFou <joan.courty@ynov.com>
2026-03-26 19:48:57 +0100

    fix(Event) : Had created all of the todo for the event with the action

--- Event.cs
+++ Event.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 public class ZooEvent
 {
@@ -10,29 +11,42 @@ public class ZooEvent
 
         if (de.Next(1, 101) <= 1)
         {
-            Console.WriteLine("[EVENT] Incendie ! Vous avez perdu 1 habitat.");
-            // TODO : monZoo.RetirerHabitat();
-            unEvenementAEuLieu = true;
+            if (monZoo.HabitatsZoo.Count > 0)
+            {
+                int indexHabitat = de.Next(monZoo.HabitatsZoo.Count);
+                monZoo.HabitatsZoo.RemoveAt(indexHabitat);
+                Console.WriteLine("[EVENT] Incendie ! Vous avez perdu 1 habitat.");
+                unEvenementAEuLieu = true;
+            }
         }
 
         if (de.Next(1, 101) <= 1)
         {
-            Console.WriteLine("[EVENT] Vol ! Un de vos animaux a disparu.");
-            // TODO : monZoo.RetirerAnimal();
-            unEvenementAEuLieu = true;
+            if (monZoo.HabitatsZoo.Count > 0)
+            {
+                var habitatCible = monZoo.HabitatsZoo[de.Next(monZoo.HabitatsZoo.Count)];
+                if (habitatCible.Animaux.Count > 0)
+                {
+                    int indexAnimal = de.Next(habitatCible.Animaux.Count);
+                    var animalVole = habitatCible.Animaux[indexAnimal];
+                    habitatCible.Animaux.RemoveAt(indexAnimal);
+                    Console.WriteLine("[EVENT] Vol ! Un de vos animaux a disparu. C'est un/une " + animalVole.GetType().Name + " !");
+                    unEvenementAEuLieu = true;
+                }
+            }
         }
 
         if (de.Next(1, 101) <= 20)
         {
             Console.WriteLine("[EVENT] Nuisibles ! Les rats ont mangé 10% de votre stock de graines.");
-            // TODO : monZoo.StockGraines = monZoo.StockGraines * 0.9m;
+            monZoo.MonStock.PerteNuisibles(); 
             unEvenementAEuLieu = true;
         }
 
         if (de.Next(1, 101) <= 10)
         {
             Console.WriteLine("[EVENT] Viande avariée ! Vous perdez 20% de votre stock de viande.");
-            // TODO : monZoo.StockViande = monZoo.StockViande * 0.8m;
+            monZoo.MonStock.PerteViandeAvariee();
             unEvenementAEuLieu = true;
         }
 
